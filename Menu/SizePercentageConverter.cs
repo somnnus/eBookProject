8d0ddc1400e605ea7b987259fb10d12c60183040
liza <Elizavetad20000102@gmail.com>
2020-10-21 21:59:42 +0300

    Гибкий книжный блок

---
+++ Menu/SizePercentageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Menu
+{
+    public class SizePercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return 0.7 * System.Convert.ToDouble(value);
+
+            string[] split = parameter.ToString().Split('.');
+            double parameterDouble = System.Convert.ToDouble(split[0]) + System.Convert.ToDouble(split[1]) / (Math.Pow(10, split[1].Length));
+            return System.Convert.ToDouble(value) * parameterDouble;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Don't need to implement this
+            return null;
+        }
+    }
+}
