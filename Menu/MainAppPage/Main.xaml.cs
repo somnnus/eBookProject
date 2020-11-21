4b45c40371d73e1aea1f93ca6d916441936edd0f
liza <Elizavetad20000102@gmail.com>
2020-11-21 18:44:25 +0300

    Расширение блока

--- Menu/MainAppPage/Main.xaml.cs
+++ Menu/MainAppPage/Main.xaml.cs
@@ -112,6 +112,11 @@ namespace Menu.MainAppPage
             }
         }
 
+        private void WrapPanel_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
         //private MultiBinding createFieldMultiBinding(string fieldName)
         //{
         //    // Create the multi-binding
