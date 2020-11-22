9806f60152c1ee57230d7b47fb7e46b556eaded1
somnus <zhenya26224@icloud.com>
2020-11-22 18:46:39 +0300

    ALMOST ALL

--- Menu/OpenedBook.xaml.cs
+++ Menu/OpenedBook.xaml.cs
@@ -16,23 +16,31 @@ using LibraryReader;
 using System.Threading;
 using Menu.SharedResources;
 using System.IO;
+using System.Windows.Markup;
+using System.Xml;
+using System.Text.RegularExpressions;
 
 namespace Menu
 {
     /// <summary>
     /// Логика взаимодействия для OpenedBook.xaml
     /// </summary>
-    
+    /// 
+    [Serializable]
     public partial class OpenedBook : Window
     {
         string fullPath = AppDomain.CurrentDomain.BaseDirectory + "Library";
 
-        string bookmark;
+        // string bookmark;
+        // int page;
 
-        Book currentBook;
-        private int columnWidth;
-        private int fontSize;
-        Paragraph paragraphHigh = new Paragraph();
+        Book currentBook = null;
+
+        private int lastPage;
+
+        string[] newText;
+
+        Paragraph paragraphHigh { get; set; }
         public OpenedBook(Book current)
         {
 
@@ -40,96 +48,75 @@ namespace Menu
             currentBook = current;
             DisplayBook();
             Serialization.SerializationLastBook(currentBook, fullPath);
+
+
+
         }
-        
-       
+
+
         public void DisplayBook()
         {
             flowDocument.Document = null;
-            Paragraph paragraph = new Paragraph();
             string text = currentBook.ReturnContent();
-            //paragraphHigh.Inlines.Add(text);
-            
-           // FlowDocument document = new FlowDocument(paragraphHigh);
+            doc.FontSize = 16;
+            newText = Regex.Split(text, @"\n");
 
-               //document.FontSize = 16;
-              //  document.ColumnRuleWidth = border.ActualWidth/2;
-             // document.ColumnWidth = 250;//3 свойства для изменения колонок! 2- одна колонка, 3 - две колонки, 4 - три колонки
-            // columnWidth = 250;
-           // document.ColumnGap = 20;
-            
-
-              //flowDocument.Document = document;
-             // FlowDocument doc =new FlowDocument();   
-            //  doc.LineHeight = 1.5;
-           
-
-            var s = Serialization.SplitPage(text, 3000);
-            
-            foreach (var paragrapg in s)
+            foreach (var paragrapg in newText)
             {
-               Paragraph p = new Paragraph();
-               p.Margin = new Thickness(0,0,0,0);
-              
-                 p.Inlines.Add(paragrapg);
-                 doc.Blocks.Add(p);
-              
+                string abz = "    " + paragrapg;
+                Paragraph p = new Paragraph();
+                p.Margin = new Thickness(0, 0, 0, 0);
+                p.Inlines.Add(abz);
+
+                doc.Blocks.Add(p);
             }
-            doc.ColumnWidth = currentBook.ColumnWidth;
-            doc.FontSize = currentBook.FontSize;
+            doc.ColumnWidth = 650;
+            if (currentBook.Zoom != -1 && currentBook.LastPage != -1)
+            {
+                flowDocument.Zoom = currentBook.Zoom;
+                lastPage = currentBook.LastPage;
+            }
+            else
+            {
+                flowDocument.Zoom = 110;
+            }
+
             flowDocument.Document = doc;
 
-       
-            
         }
-        
-        private void CreateBookmark(object sender,RoutedEventArgs routedEventArgs)
+
+        private void CreateBookmark(object sender, RoutedEventArgs routedEventArgs)
         {
             Bookmark mark = new Bookmark();
-          //  mark.NumberPage = flowDocument.MasterPageNumber;
-            mark.FrontSize = flowDocument.FontSize;
-            mark.ColumnWidth = columnWidth;
+            mark.NumberPage = flowDocument.MasterPageNumber;
             currentBook.AddBookmark(mark);
-            
-
-            var paginator = ((IDocumentPaginatorSource)flowDocument.Document).DocumentPaginator as DynamicDocumentPaginator;
-            var position = paginator.GetPagePosition(paginator.GetPage(flowDocument.PageNumber)) as TextPointer;
-             paragraphHigh = position.Paragraph;
-            //  string text = paragraphHigh.ContentEnd
-            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(Paragraph));
-            using (var writer = new StreamWriter(@"C:\Users\user\Desktop\Проект\eBookProject\Menu\bin\Debug\Library\mark.xml"))
-            {
-                serializer.Serialize(writer, paragraphHigh);
-            }
-
-
-            TextRange text = new TextRange(paragraphHigh.ContentStart, paragraphHigh.ContentEnd);
-            bookmark = text.Text;
-            Serialization.SerializationInformationAboutBook(ResourcesProvider.Current.ListBooks, fullPath);
 
         }
         private void OpenBookmark(object sender, RoutedEventArgs routedEventArgs)
         {
-            if (currentBook.bookmarks.Count!=0)
+            if (currentBook.bookmarks.Count != 0)
             {
-                Paragraph p = new Paragraph();
-               // p.Margin = new Thickness(0, 0, 0, 0);
-                p.Inlines.Add(bookmark);
-                p.BringIntoView();
-                flowDocument.BringIntoView();
-                //paragraphHigh.BringIntoView();
+                flowDocument.GoToPage(currentBook.bookmarks[0].NumberPage);
             }
         }
 
-        
-        private void SearchInBook(object sender, RoutedEventArgs routedEventArgs)
+        private void FindInBook(object sender, RoutedEventArgs routedEventArgs)
         {
             flowDocument.Find();
         }
+
+        private void ContinueReading(object sender, RoutedEventArgs routedEventArgs)
+        {
+
+            flowDocument.GoToPage(currentBook.LastPage);
+        }
+
         private void OpenMenu(object sender, RoutedEventArgs routedEventArgs)
         {
-            currentBook.FontSize = flowDocument.Document.FontSize;
-            currentBook.ColumnWidth = flowDocument.Document.ColumnWidth;
+            currentBook.LastPage = flowDocument.MasterPageNumber;
+            currentBook.Zoom = flowDocument.Zoom;
+
+            Serialization.SerializationLastBook(currentBook, fullPath);
             Serialization.SerializationInformationAboutBook(ResourcesProvider.Current.ListBooks, fullPath);
             var menuWindow = new MainWindow(ResourcesProvider.Current.MainWindowVM);
             menuWindow.Show();
